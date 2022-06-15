0907a90a8d75f136f6d2c8e67b6b57b9d4ee5d59
Alexey Kelin <0072112@mail.ru>
2022-06-15 19:49:57 +0200

    'WTF'

--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -5,7 +5,9 @@ from django.template import loader
 # Главная страница
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title= 'Это мой Title'
+    context = {'title':title, 'text':'Это главная страница проекта Yatube'}
+    return render(request, template, context)
 
 def group_posts(request):
     return HttpResponse('Список постов')
