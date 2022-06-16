dc39125e99ae8a38ade2cf8277b39287019c4a2a
Alexey Kelin <0072112@mail.ru>
2022-06-16 09:51:03 +0200

    'WTF'

--- Yatube/posts/urls.py
+++ Yatube/posts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('group/', views.group_posts),
-    path('group/<slug:pk>/', views.group_posts_detail)
+    path('group/<slug:pk>/', views.group_posts_detail, name='group list')
 ]
