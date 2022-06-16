b49c696f0815a5c161fa60d99b90a51bed230b26
Alexey Kelin <0072112@mail.ru>
2022-06-16 17:22:18 +0200

    'WTF'

--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -1,18 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
-COUNT_RECORDS: int = 10
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+# Главная страница
 def index(request):
-    posts = Post.objects.select_related('author', 'group').all()[:COUNT_RECORDS]
-    context = {
-        'posts': posts,
-    }
+    context = {'text':'Это главная страница проекта Yatube'}
     return render(request, 'posts/index.html', context)
+
 def group_posts(request, slug):
-    group = get_object_or_404(Group, slug=slug)
-    group_posts = group.posts.all()[:COUNT_RECORDS]
-    context = {
-        'group': group,
-        'group_posts': group_posts,
-    }
+    context = { 'title':'Это главная страница проекта group Yatube'}
     return render(request, 'posts/group_list.html', context)
 
