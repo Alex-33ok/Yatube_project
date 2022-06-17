dbd6d9181a5c0b0b8288ad2430218bb053c209a4
Alexey Kelin <0072112@mail.ru>
2022-06-17 11:40:37 +0200

    'WTF'

--- Yatube/posts/models.py
+++ Yatube/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    start_at = models.DateTimeField()
+    description = models.TextField()
+    contact=models.EmailField(max_length=200)
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    location= models.CharField(max_length=400)
--- Yatube/posts/urls.py
+++ Yatube/posts/urls.py
@@ -1,7 +1,7 @@
 # Yatube/posts/urls.py
 from django.urls import path
 from . import views
-
+app_name='posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('group/<slug:pk>/', views.group_posts, name='group_list')
