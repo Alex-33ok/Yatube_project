a8c3d83c0f1d73dd5b7f118abd634b4b86f61dfb
Alexey Kelin <0072112@mail.ru>
2022-06-18 18:48:34 +0200

    'Migrations made'

--- Yatube/posts/admin.py
+++ Yatube/posts/admin.py
@@ -1,6 +1,21 @@
-
-# Register your models here.
 from django.contrib import admin
-from .models import Post
 
-admin.site.register(Post)
+from .models import Post, Group
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("pk", "title", "slug")
+    search_fields = ("title", "slug")
+    list_filter = ("slug",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- Yatube/posts/models.py
+++ Yatube/posts/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -14,11 +20,17 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
 
-class Event(models.Model):
-    name = models.CharField(max_length=200)
-    start_at = models.DateTimeField()
-    description = models.TextField()
-    contact=models.EmailField(max_length=200)
-    author=models.ForeignKey(User, on_delete=models.CASCADE)
-    location= models.CharField(max_length=400)
+#class Event(models.Model):
+    #name = models.CharField(max_length=200)
+    #start_at = models.DateTimeField()
+    #description = models.TextField()
+    #contact=models.EmailField(max_length=200)
+    #author=models.ForeignKey(User, on_delete=models.CASCADE)
+    #location= models.CharField(max_length=400)
