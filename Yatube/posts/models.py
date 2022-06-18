f912b6b3b8bb7e7e139f75ddfe973cea31455681
Alexey Kelin <0072112@mail.ru>
2022-06-18 10:35:14 +0200

    Yes

--- Yatube/posts/models.py
+++ Yatube/posts/models.py
@@ -4,6 +4,22 @@ from django.db import models
 from django.db import models
 from django.contrib.auth import get_user_model
 
+#ВОТ ЭТО Я УКРАЛ------->>>>>:
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+
 User = get_user_model()
 class Event(models.Model):
     name = models.CharField(max_length=200)
