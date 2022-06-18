5c7ba4a4ec43bbff0198b696c8dfc4a863e489fe
Alexey Kelin <0072112@mail.ru>
2022-06-18 11:06:46 +0200

    'WTF'

--- Yatube/posts/models.py
+++ Yatube/posts/models.py
@@ -4,7 +4,8 @@ from django.db import models
 from django.db import models
 from django.contrib.auth import get_user_model
 
-#ВОТ ЭТО Я УКРАЛ------->>>>>:
+User = get_user_model()
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -13,14 +14,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
-    group = models.ForeignKey(
-        Group,
-        blank=True,
-        null=True,
-        on_delete=models.CASCADE
-    )
 
-User = get_user_model()
 class Event(models.Model):
     name = models.CharField(max_length=200)
     start_at = models.DateTimeField()
